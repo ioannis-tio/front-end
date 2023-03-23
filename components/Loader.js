ce98ed2a168265b5a6fda8dcab0c569e06b86b94
Ioannis <ioannis.k@tiomarkets.com>
2023-03-23 17:31:11 +0200

    hello 2

---
+++ components/Loader.js
@@ -0,0 +1,39 @@
+import React from "react";
+import styled from "styled-components";
+
+const LoaderContainer = styled.div`
+  .loading-spinner {
+    width: 50px;
+    height: 50px;
+    border: 10px solid #f3f3f3; /* Light grey */
+    border-top: 10px solid #383636; /* Blue */
+    border-radius: 50%;
+    animation: spinner 1.5s linear infinite;
+  }
+  @keyframes spinner {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+`;
+const LoaderSpinner = styled.div`
+  .loading-spinner {
+    width: 50px;
+    height: 50px;
+    border: 10px solid #f3f3f3; /* Light grey */
+    border-top: 10px solid #383636; /* Blue */
+    border-radius: 50%;
+    animation: spinner 1.5s linear infinite;
+  }
+`;
+
+export default function Loader() {
+  return (
+    <LoaderContainer className="spinner-container">
+      <LoaderSpinner className="loading-spinner"></LoaderSpinner>
+    </LoaderContainer>
+  );
+}
---
+++ config/colors.js
@@ -0,0 +1,5 @@
+export const colors = {
+  black: "#000000",
+  textC: "#233142",
+  bgColor: "#fafafa",
+};
